1dfea703361f6ff5d14180eaf46beabee5dbdd62
Raúl García <raulpeugeot@gmail.com>
2025-06-30 17:29:13 +0000

    [review] style code acording PEP8

--- src/api/models.py
+++ src/api/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -14,8 +14,21 @@ class User(db.Model):
         return f'<User {self.email}>'
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a secur
+        # do not serialize the password, its a security breach
+        return { "id": self.id,
+                 "email": self.email,
+        "is active": self.is_active}
+            
+            
+    class Post(db.Model):
+        id = db.Colum(db.Integer, primary_key=True)
+        title = db.Colum8(db.String(), nullable=True)
+        descriotion = db.Colum(db.String(), nullable=True)
+        body =  db.Colum(db.String(), nullable=True)
+        date =  db.Colum(db.date())
+        image_url = db.Colum(db.String())
 
+       class Medias(db.Model):
+        id = db.Colum(db.Integer, primary_key=True)
+        type = db.Colum8(db.enum(), nullable=True)
+        image_url = db.Colum(db.String())  
--- src/api/routes.py
+++ src/api/routes.py
@@ -8,10 +8,10 @@ from api.models import db, Users
 
 
 api = Blueprint('api', __name__)
-CORS(api)# Allow CORS requests to this API
+CORS(api)  # Allow CORS requests to this API
 
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
-    response_body = { "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"}
-    return jsonify(response_body), 200
+    response_body = { "message": "Hello! I'm a message that came from the backend"}
+    return response_body, 200
